1b81e1f31f0be201952d8472b8a9db4f9e0136d3
Hiago Dev <hiagocavalcante03@gmail.com>
2024-09-30 19:22:30 -0300

    implamentando a Api

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const moviesContainer = document.querySelector('.content');
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGE0NDQwYjQ0Y2M5ODJkNTc3ZTJlYzJlZWUxNDI1OCIsIm5iZiI6MTcyNzczMzQ5MC4xMDU2NTQsInN1YiI6IjY2ZmIxMzllODA3Y2Q1MWMxN2YxYWEwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZsQ5oo1L7VHa1byCj-plW-2s1GEb452ZLJ0U_h61bqU',
+  },
+};
+
+const renderMovies = function (data, classname = '') {
+  const html = `
+    <div class="movie-grid">
+    <div class="movie-card">
+    <img src="${data}" alt="Filme 1" />
+    <div class="movie-info">
+    <h4>Filme 1</h4>
+    </div>
+    </div>
+    <div class="movie-card">
+    <img src="${data}" alt="Filme 2" />
+    <div class="movie-info">
+    <h4>Filme 2</h4>
+    </div>
+    </div>
+    <div class="movie-card">
+    <img src="${data}" alt="Filme 3" />
+    <div class="movie-info">
+    <h4>Filme 3</h4>
+    </div>
+    </div>
+    <div class="movie-card">
+    <img src="${data}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>Filme 4</h4>
+    </div>
+    </div>
+    </div>
+    </div>`;
+
+  moviesContainer.insertAdjacentHTML('beforeend', html);
+};
+
+const getMovies = function () {
+  fetch('https://api.themoviedb.org/3/configuration', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+};
