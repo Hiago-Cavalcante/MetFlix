85434fa759e66b975bc7d1dbe54ab2a1535800ca
Hiago Dev <hiagocavalcante03@gmail.com>
2024-10-03 22:48:47 -0300

    36

--- main.js
+++ main.js
@@ -27,28 +27,101 @@ const renderMovies = function (data) {
     <div class="movie-card">
     <img src="https://image.tmdb.org/t/p/w500${data.results[0].poster_path}" alt="Filme 1" />
     <div class="movie-info">
-    <h4>Filme 1</h4>
+    <h4>${data.results[0].original_title}</h4>
     </div>
     </div>
     <div class="movie-card">
     <img src="https://image.tmdb.org/t/p/w500${data.results[1].poster_path}" alt="Filme 2" />
     <div class="movie-info">
-    <h4>Filme 2</h4>
+    <h4>${data.results[1].original_title}</h4>
     </div>
     </div>
     <div class="movie-card">
     <img src="https://image.tmdb.org/t/p/w500${data.results[2].poster_path}" alt="Filme 3" />
     <div class="movie-info">
-    <h4>Filme 3</h4>
+    <h4>${data.results[2].original_title}</h4>
     </div>
     </div>
     <div class="movie-card">
     <img src="https://image.tmdb.org/t/p/w500${data.results[5].poster_path}" alt="Filme 4" />
     <div class="movie-info">
-    <h4>Filme 4</h4>
+    <h4>${data.results[5].original_title}</h4>
     </div>
     </div>
+    <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[3].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[3].original_title}</h4>
     </div>
+    </div>
+    <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[4].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[4].original_title}</h4>
+    </div>
+    </div>
+    <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[8].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[8].original_title}</h4>
+    </div>
+    </div>
+    <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[10].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[10].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[6].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[6].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[7].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[7].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[11].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[11].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[12].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[12].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[13].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[13].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[14].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[14].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[15].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[15].original_title}</h4>
+    </div>
+    </div>
+     <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${data.results[16].poster_path}" alt="Filme 4" />
+    <div class="movie-info">
+    <h4>${data.results[16].original_title}</h4>
+    </div>
+    </div>
+</div>
+
     </div>`;
 
   moviesContainer.insertAdjacentHTML('beforeend', html);
