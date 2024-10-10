38cd3c070ae99dcbfbdf697b9109087032e6122d
Hiago Dev <hiagocavalcante03@gmail.com>
2024-10-10 18:54:15 -0300

    new render movie logic

--- main.js
+++ main.js
@@ -2,129 +2,45 @@
 
 const moviesContainer = document.querySelector('.content');
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGE0NDQwYjQ0Y2M5ODJkNTc3ZTJlYzJlZWUxNDI1OCIsIm5iZiI6MTcyNzczMzQ5MC4xMDU2NTQsInN1YiI6IjY2ZmIxMzllODA3Y2Q1MWMxN2YxYWEwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZsQ5oo1L7VHa1byCj-plW-2s1GEb452ZLJ0U_h61bqU',
-  },
-};
+const apiKey = '9b51dd245f1267676721b04d01a8e806';
+const baseUrl = 'https://api.themoviedb.org/3/movie/popular';
+let page = 1;
 
-const getMovies = function () {
-  fetch('https://api.themoviedb.org/3/discover/movie', options)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      renderMovies(data);
-    })
-    .catch(err => console.error(err));
+const getMovies = async function () {
+  try {
+    const data = await fetch(
+      `${baseUrl}?api_key=${apiKey}&language=pt-BR&page=${page}`
+    );
+    const res = await data.json();
+
+    renderMovies(res);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const renderMovies = function (data) {
-  const html = `
-    <div class="movie-grid">
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[0].poster_path}" alt="Filme 1" />
-    <div class="movie-info">
-    <h4>${data.results[0].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[1].poster_path}" alt="Filme 2" />
-    <div class="movie-info">
-    <h4>${data.results[1].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[2].poster_path}" alt="Filme 3" />
-    <div class="movie-info">
-    <h4>${data.results[2].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[5].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[5].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[3].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[3].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[4].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[4].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[8].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[8].original_title}</h4>
-    </div>
-    </div>
-    <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[10].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[10].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[6].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[6].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[7].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[7].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[11].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[11].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[12].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[12].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[13].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[13].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[14].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[14].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[15].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[15].original_title}</h4>
-    </div>
-    </div>
-     <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w500${data.results[16].poster_path}" alt="Filme 4" />
-    <div class="movie-info">
-    <h4>${data.results[16].original_title}</h4>
-    </div>
-    </div>
-</div>
+  let htmlContent = '<div class="movie-grid">';
 
-    </div>`;
+  for (let i = 0; i < data.results.length && i < 16; i++) {
+    const movie = data.results[i];
+    const vote = data.results[i].vote_average;
+    htmlContent += `
+    <div class="movie-card">
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Filme ${
+      i + 1
+    }" />
+    <div class="movie-info">
+    <h4>${movie.original_title}</h4>
+    <span class="movie-avarage"> ${vote.toFixed(1)} 
+    </div>
+    </div>
+    `;
+  }
 
-  moviesContainer.insertAdjacentHTML('beforeend', html);
+  htmlContent += '</div>';
+
+  moviesContainer.insertAdjacentHTML('beforeend', htmlContent);
 };
 
 getMovies();
