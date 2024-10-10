501c30d98f741d73841bb599681a5eb76f3ace13
Hiago Dev <hiagocavalcante03@gmail.com>
2024-10-10 19:50:51 -0300

    New Changes n1

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const moviesContainer = document.querySelector('.content');
+const pageBtnPrevious = document.querySelector('.prevPage');
+const pageBtnNext = document.querySelector('.nextPage');
 
 const apiKey = '9b51dd245f1267676721b04d01a8e806';
 const baseUrl = 'https://api.themoviedb.org/3/movie/popular';
@@ -8,9 +10,8 @@ let page = 1;
 
 const getMovies = async function () {
   try {
-    const data = await fetch(
-      `${baseUrl}?api_key=${apiKey}&language=pt-BR&page=${page}`
-    );
+    const data = await fetch(`${baseUrl}?api_key=${apiKey}&page=${page}`);
+
     const res = await data.json();
 
     renderMovies(res);
@@ -20,6 +21,8 @@ const getMovies = async function () {
 };
 
 const renderMovies = function (data) {
+  moviesContainer.innerHTML = '';
+
   let htmlContent = '<div class="movie-grid">';
 
   for (let i = 0; i < data.results.length && i < 16; i++) {
@@ -43,4 +46,31 @@ const renderMovies = function (data) {
   moviesContainer.insertAdjacentHTML('beforeend', htmlContent);
 };
 
+
+const pageBtns = function () {
+  pageBtnNext.addEventListener('click', function (e) {
+    e.preventDefault();
+    page++;
+    getMovies();
+    updateBtns();
+  });
+  
+  pageBtnPrevious.addEventListener('click', function (e) {
+    e.preventDefault();
+    page--;
+    getMovies();
+    updateBtns();
+  });
+};
+
+
+const updateBtns = function () {
+  if (page > 1) {
+    pageBtnPrevious.removeAttribute('disabled');
+  } else {
+    pageBtnPrevious.setAttribute('disabled', true);
+  }
+};
+
 getMovies();
+pageBtns();
